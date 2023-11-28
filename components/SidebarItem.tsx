e54559909870d3fc8e1fd81ed2cc41cce7eb0156
Van Phung <vanphung030792@gmail.com>
2023-11-28 02:44:53 -0500

    first commit

---
+++ components/SidebarItem.tsx
@@ -0,0 +1,34 @@
+import { IconType } from "react-icons/lib/esm/iconBase";
+
+interface SidebarItemProps {
+    label: string;
+    href: string;
+    icon: IconType;
+    onClick: () => void;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({ label, href, icon: Icon, onClick }) => {
+  return (
+    <div className="flex flex-row items-center">
+        <div
+            className="
+                relative
+                rounded-full
+                h-14
+                w-14
+                flex
+                items-center
+                justify-center
+                p-4
+                hover:bg-slate-300
+                hover:bg-opacity-10
+                cursor-pointer
+                lg:hidden
+            "
+        >
+            <Icon size={28} color="white"></Icon>
+        </div>
+    </div>
+  )
+}
+export default SidebarItem
